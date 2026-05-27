605d6fc3c4cd7557795159cb151bfba9497e06d2
claudiu.badilas <claudiu.badilas@gmail.com>
2026-05-27 18:10:35 +0300

    update key

--- Predict.Client/src/app/modules/transaction/services/transaction.service.ts
+++ Predict.Client/src/app/modules/transaction/services/transaction.service.ts
@@ -9,7 +9,7 @@ import {
   TransactionResponse,
 } from '../models/transactions.model';
 
-export const TransactionService_STORAGE_KEY = 'Transactions_Cache';
+export const TransactionService_STORAGE_KEY = 'Transactions_Cache_May_2025';
 
 @Injectable({ providedIn: 'root' })
 export class TransactionService {
