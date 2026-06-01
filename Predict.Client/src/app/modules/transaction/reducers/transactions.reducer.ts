5749b483c8417edd47588d496250e930e7fa723c
Claudiu-Badilas <71487855+Claudiu-Badilas@users.noreply.github.com>
2026-06-01 21:23:50 +0300

    Update transactions.reducer.ts

--- Predict.Client/src/app/modules/transaction/reducers/transactions.reducer.ts
+++ Predict.Client/src/app/modules/transaction/reducers/transactions.reducer.ts
@@ -21,7 +21,7 @@ export interface State {
 
 const initialState: State = {
   transactions: [],
-  startDate: new Date('17-12-2017),
+  startDate: new Date('2017-12-17),
   endDate: new Date(),
   selectedProvider: 'RAIFFEISEN',
   selectedServiceProvider: 'No Selection',
