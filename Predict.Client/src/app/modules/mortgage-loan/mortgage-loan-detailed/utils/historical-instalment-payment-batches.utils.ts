c8c7eb385f14ce3955ba68ef6b71bae87ecc67c1
claudiu.badilas <claudiu.badilas@gmail.com>
2026-06-06 09:59:13 +0300

    fix index

--- Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-detailed/utils/historical-instalment-payment-batches.utils.ts
+++ Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-detailed/utils/historical-instalment-payment-batches.utils.ts
@@ -12,22 +12,24 @@ export namespace HistoricalInstalmentPaymentBatchesUtils {
     const batches: HistoricalInstalmentPaymentBatch[] = [];
     let tempBatch: HistoricalInstalmentPayment[] = [];
 
-    overviewBaseLoanInstalments.forEach((current, index, array) => {
-      const next = array[index + 1];
+    overviewBaseLoanInstalments
+      .map((instalment, i) => ({ ...instalment, index: i + 1 }))
+      .forEach((current, index, array) => {
+        const next = array[index + 1];
 
-      tempBatch.push(current);
+        tempBatch.push(current);
 
-      if (current.instalmentPayment || current.earlyPayment) {
-        if (next && !next.earlyPayment) {
+        if (current.instalmentPayment || current.earlyPayment) {
+          if (next && !next.earlyPayment) {
+            batches.push(new HistoricalInstalmentPaymentBatch(tempBatch));
+            tempBatch = [];
+          }
+        }
+        if ((!current.instalmentPayment && !current.earlyPayment) || !next) {
           batches.push(new HistoricalInstalmentPaymentBatch(tempBatch));
           tempBatch = [];
         }
-      }
-      if ((!current.instalmentPayment && !current.earlyPayment) || !next) {
-        batches.push(new HistoricalInstalmentPaymentBatch(tempBatch));
-        tempBatch = [];
-      }
-    });
+      });
 
     return batches;
   }
