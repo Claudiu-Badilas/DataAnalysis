fb1334e585eb6351831a73f94eb435ccbca065ca
claudiu.badilas <claudiu.badilas@gmail.com>
2026-07-19 08:47:38 +0300

    remove banner

--- Predict.Client/src/app/app.component.ts
+++ Predict.Client/src/app/app.component.ts
@@ -1,10 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastNotificationComponent } from './platform/toast-notifications/toast-notification.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
-import { VersionService } from './shared/services/version.service';
-import { NewVersionBannerComponent } from './shared/components/new-version-banner/new-version-banner.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'p-root',
@@ -12,14 +10,12 @@ import { HttpClientModule } from '@angular/common/http';
     RouterModule,
     SpinnerComponent,
     ToastNotificationComponent,
-    NewVersionBannerComponent,
     HttpClientModule,
   ],
-  providers: [VersionService],
+  providers: [],
   template: `
     <p-spinner />
     <p-toast />
-    <app-new-version-banner />
     <router-outlet />
   `,
   styleUrls: ['app.component.scss'],
