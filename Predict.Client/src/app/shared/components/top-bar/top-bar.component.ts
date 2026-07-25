0da9e3d2457afb3f885b3b6cf9a92568f17cf9fb
claudiu.badilas <claudiu.badilas@gmail.com>
2026-07-25 10:21:22 +0300

    remove click outside logic

--- Predict.Client/src/app/shared/components/top-bar/top-bar.component.ts
+++ Predict.Client/src/app/shared/components/top-bar/top-bar.component.ts
@@ -1,13 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  Input,
-  OnInit,
-  Renderer2,
-} from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import * as NavigationAction from 'src/app/store/actions/navigation.actions';
@@ -19,7 +11,7 @@ import * as fromAppStore from 'src/app/store/app-state.reducer';
   styleUrl: './top-bar.component.scss',
   imports: [CommonModule],
 })
-export class TopBarComponent implements OnInit, AfterViewInit {
+export class TopBarComponent implements OnInit {
   @Input() hasModuleNavContent: boolean = false;
   @Input() hasFiltersContent: boolean = false;
 
@@ -35,15 +27,11 @@ export class TopBarComponent implements OnInit, AfterViewInit {
   isFilterPanelOpen = false;
   isAnimating = false;
   currentUrl: string = '';
-  private animationTimeout: any;
-  private isClickOutsideEnabled = true;
 
   constructor(
     private store: Store<fromAppStore.AppState>,
     private router: Router,
     private cdr: ChangeDetectorRef,
-    private elementRef: ElementRef,
-    private renderer: Renderer2,
   ) {}
 
   ngOnInit() {
@@ -51,13 +39,6 @@ export class TopBarComponent implements OnInit, AfterViewInit {
 
     this.router.events.subscribe(() => {
       this.currentUrl = this.router.url;
-    });
-  }
-
-  ngAfterViewInit() {
-    // Add click outside listener
-    this.renderer.listen('document', 'click', (event: Event) => {
-      this.handleClickOutside(event);
     });
   }
 
@@ -69,7 +50,7 @@ export class TopBarComponent implements OnInit, AfterViewInit {
   }
 
   toggleMenu() {
-    // If filter panel is open, just close it and don't toggle the menu
+    // If filter panel is open, close it and don't toggle the menu
     if (this.isFilterPanelOpen) {
       this.closeFilterPanel();
       return;
@@ -120,64 +101,5 @@ export class TopBarComponent implements OnInit, AfterViewInit {
 
   onNavigateTo(url: any) {
     this.store.dispatch(NavigationAction.navigateTo({ route: url }));
-  }
-
-  private handleClickOutside(event: Event) {
-    const target = event.target as HTMLElement;
-
-    // Get references to the elements
-    const topbarElement =
-      this.elementRef.nativeElement.querySelector('.topbar');
-    const mobileMenuElement = this.elementRef.nativeElement.querySelector(
-      '.mobile-extended-menu',
-    );
-    const filterPanelElement =
-      this.elementRef.nativeElement.querySelector('.filter-panel');
-    const burgerButton =
-      this.elementRef.nativeElement.querySelector('.burger-btn');
-    const filterButton =
-      this.elementRef.nativeElement.querySelector('.filter-btn');
-
-    // Check if click is outside the topbar component entirely
-    const isClickInsideComponent =
-      this.elementRef.nativeElement.contains(target);
-
-    if (!isClickInsideComponent) {
-      // Click is outside the entire component - close everything
-      if (this.isMenuOpen) {
-        this.closeMenu();
-      }
-      if (this.isFilterPanelOpen) {
-        this.closeFilterPanel();
-      }
-      this.cdr.detectChanges();
-      return;
-    }
-
-    // Check if click is on the burger button or mobile menu
-    const isBurgerButton = burgerButton?.contains(target);
-    const isMobileMenu = mobileMenuElement?.contains(target);
-
-    // Check if click is on the filter button or filter panel
-    const isFilterButton = filterButton?.contains(target);
-    const isFilterPanel = filterPanelElement?.contains(target);
-
-    // If menu is open and click is outside the mobile menu and not on the burger button
-    if (this.isMenuOpen && !isMobileMenu && !isBurgerButton) {
-      // Check if click is on filter elements - if so, don't close menu (let filter toggle handle it)
-      if (!isFilterButton && !isFilterPanel) {
-        this.closeMenu();
-        this.cdr.detectChanges();
-      }
-    }
-
-    // If filter panel is open and click is outside the filter panel and not on the filter button
-    if (this.isFilterPanelOpen && !isFilterPanel && !isFilterButton) {
-      // Check if click is on mobile menu elements - if so, don't close filter (let menu toggle handle it)
-      if (!isMobileMenu && !isBurgerButton) {
-        this.closeFilterPanel();
-        this.cdr.detectChanges();
-      }
-    }
   }
 }
