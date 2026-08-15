b3b941d9e3812b9097d8754d38ba32daa672d2ad
claudiu.badilas <claudiu.badilas@gmail.com>
2026-08-15 09:08:56 +0300

    refactor

--- Predict.Client/src/main.ts
+++ Predict.Client/src/main.ts
@@ -1,34 +1,33 @@
 import {
   enableProdMode,
-  provideZoneChangeDetection,
   importProvidersFrom,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-import { AppRouting } from './app/app.routing';
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import * as fromAppStore from 'src/app/store/app-state.reducer';
-import { NavigationEffects } from 'src/app/store/effects/navigation.effects';
+import { StoreModule } from '@ngrx/store';
 import { AuthenticationEffects } from 'src/app/platform/authentication/effects/authentication.effects';
 import { ToastNotificationEffects } from 'src/app/platform/toast-notifications/effects/toast-notification.effects';
+import * as fromAppStore from 'src/app/store/app-state.reducer';
+import { NavigationEffects } from 'src/app/store/effects/navigation.effects';
+import { AppComponent } from './app/app.component';
+import { AppRouting } from './app/app.routing';
+import { environment } from './environments/environment';
 
 // Feature states & effects
-import * as fromInvoices from 'src/app/modules/invoices/reducers/invoices.reducer';
-import { InvoicesEffects } from 'src/app/modules/invoices/effects/invoices.effects';
-import * as fromReceipts from 'src/app/modules/receipts/reducers/receipts.reducer';
-import { ReceiptsEffects } from 'src/app/modules/receipts/effects/receipts.effects';
-import * as fromTransactions from 'src/app/modules/transaction/reducers/transactions.reducer';
-import { TransactionsEffects } from 'src/app/modules/transaction/effects/transactions.effects';
-import * as fromLoan from 'src/app/modules/loan/reducers/loan.reducer';
-import { LoanEffects } from 'src/app/modules/loan/effects/loan.effects';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthenticationInterceptor } from 'src/app/platform/authentication/interceptor/authentication.interceptor';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InvoicesEffects } from 'src/app/modules/invoices/effects/invoices.effects';
+import * as fromInvoices from 'src/app/modules/invoices/reducers/invoices.reducer';
+import { LoanEffects } from 'src/app/modules/loan/effects/loan.effects';
+import * as fromLoan from 'src/app/modules/loan/reducers/loan.reducer';
+import { ReceiptsEffects } from 'src/app/modules/receipts/effects/receipts.effects';
+import * as fromReceipts from 'src/app/modules/receipts/reducers/receipts.reducer';
+import { TransactionsEffects } from 'src/app/modules/transaction/effects/transactions.effects';
+import * as fromTransactions from 'src/app/modules/transaction/reducers/transactions.reducer';
+import { AuthenticationInterceptor } from 'src/app/platform/authentication/interceptor/authentication.interceptor';
 
 if (environment.production) {
   enableProdMode();
@@ -50,24 +49,21 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(NgbModule),
     // Feature stores
     importProvidersFrom(
-      StoreModule.forFeature('InvoicesState', fromInvoices.reducer),
+      EffectsModule.forFeature([
+        InvoicesEffects,
+        ReceiptsEffects,
+        TransactionsEffects,
+        LoanEffects,
+      ]),
     ),
-    importProvidersFrom(EffectsModule.forFeature([InvoicesEffects])),
 
-    importProvidersFrom(
-      StoreModule.forFeature('ReceiptsState', fromReceipts.reducer),
-    ),
-    importProvidersFrom(EffectsModule.forFeature([ReceiptsEffects])),
-
-    importProvidersFrom(
+    importProvidersFrom([
+      StoreModule.forFeature('LoanState', fromLoan.reducer),
       StoreModule.forFeature('TransactionsState', fromTransactions.reducer),
-    ),
-    importProvidersFrom(EffectsModule.forFeature([TransactionsEffects])),
+      StoreModule.forFeature('ReceiptsState', fromReceipts.reducer),
+      StoreModule.forFeature('InvoicesState', fromInvoices.reducer),
+    ]),
 
-    importProvidersFrom(StoreModule.forFeature('LoanState', fromLoan.reducer)),
-    importProvidersFrom(EffectsModule.forFeature([LoanEffects])),
-
-    // Global providers (interceptors, etc.)
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthenticationInterceptor,
